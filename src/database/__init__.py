3fc340050eb5800bfe948919b57bbf4948f7266d
Alon (PC) <downtown2u@gmail.com>
2020-12-28 22:15:35 +0200

    Initial commit

---
+++ src/database/__init__.py
@@ -0,0 +1,6 @@
+"""
+In this folder, you will find scripts that help to manage the other scripts
+in the project. The database makes sure a user doesn't post the same submission
+twice, and in general, keeps track of the automated actions of the users to make
+sure the bot doesn't spam the API and behaves like a normal user. (:
+"""
---
+++ src/submit/__init__.py
@@ -0,0 +1,4 @@
+"""
+In this folder, you will find scripts that post new submission to reddit,
+using the reddit api. Submissions can be memes, news articles, and more!
+"""
